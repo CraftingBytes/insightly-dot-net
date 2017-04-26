96d8d605059afcb3989eda84961cb114ebaeb59f
Scott Reed <scott@craftingbytes.com>
2017-04-25 19:59:49 -0700

    update to latest JSON in preparation for updating to API v 2.2

--- Insightly-SDK/InsightlyRequest.cs
+++ Insightly-SDK/InsightlyRequest.cs
@@ -5,34 +5,40 @@ using System.Net;
 using System.Text;
 using Newtonsoft.Json;
 
-namespace InsightlySDK{
-	public enum HTTPMethod {
+namespace InsightlySDK
+{
+	public enum HTTPMethod
+	{
 		GET,
 		PUT,
 		POST,
 		DELETE
 	}
-	
-	public class InsightlyRequest{
-		public InsightlyRequest(string api_key, string url_path){
+
+	public class InsightlyRequest
+	{
+		public InsightlyRequest(string api_key, string url_path)
+		{
 			this.method = HTTPMethod.GET;
 			this.api_key = api_key;
 			this.url_path = url_path;
 			this.query_params = new List<string>();
 		}
-		
-		public Stream AsInputStream(){
+
+		public Stream AsInputStream()
+		{
 			var url = new UriBuilder(BASE_URL);
 			url.Path = this.url_path;
-            url.Query = this.QueryString;
-			
+			url.Query = this.QueryString;
+
 			var request = WebRequest.Create(url.ToString());
 			request.Method = this.method.ToString();
-			
+
 			var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(api_key + ":"));
 			request.Headers.Add("Authorization", "Basic " + credentials);
-			
-			if(this.body != null){
+
+			if (this.body != null)
+			{
 				request.ContentLength = Encoding.UTF8.GetByteCount(this.body);
 				request.ContentType = "application/json";
 				var writer = new StreamWriter(request.GetRequestStream());
@@ -40,91 +46,111 @@ namespace InsightlySDK{
 				writer.Flush();
 				writer.Close();
 			}
-			
+
 			var response = request.GetResponse();
 			return response.GetResponseStream();
 		}
-		
-		public Object AsJson(){
+
+		public Object AsJson()
+		{
 			return this.AsJson<Object>();
 		}
-		
-		public T AsJson<T>(){
+
+		public T AsJson<T>()
+		{
 			return JsonConvert.DeserializeObject<T>(this.AsString());
 		}
-		
-		public string AsString(){
+
+		public string AsString()
+		{
 			return (new StreamReader(this.AsInputStream())).ReadToEnd();
 		}
-		
-		public InsightlyRequest WithBody(string contents){
+
+		public InsightlyRequest WithBody(string contents)
+		{
 			this.body = contents;
 			return this;
 		}
-		
-		public InsightlyRequest WithBody<T>(T body){
+
+		public InsightlyRequest WithBody<T>(T body)
+		{
 			return this.WithBody(JsonConvert.SerializeObject(body));
 		}
-		
-		public InsightlyRequest WithQueryParam(string name, string value){
+
+		public InsightlyRequest WithQueryParam(string name, string value)
+		{
 			this.query_params.Add(Uri.EscapeDataString(name) + "=" + Uri.EscapeDataString(value));
 			return this;
 		}
-		
-		public InsightlyRequest WithQueryParam(string name, int val){
+
+		public InsightlyRequest WithQueryParam(string name, int val)
+		{
 			this.WithQueryParam(name, val.ToString());
 			return this;
 		}
-		
-		public InsightlyRequest WithQueryParam(string name, List<int> values){
-			foreach(var val in values){
+
+		public InsightlyRequest WithQueryParam(string name, List<int> values)
+		{
+			foreach (var val in values)
+			{
 				this.WithQueryParam(name, val);
 			}
 			return this;
 		}
-		
-		public InsightlyRequest WithMethod(HTTPMethod method){
+
+		public InsightlyRequest WithMethod(HTTPMethod method)
+		{
 			this.method = method;
 			return this;
 		}
 
-		
+
 		// OData Query building methods
-		
-		public InsightlyRequest Top(int n){
+
+		public InsightlyRequest Top(int n)
+		{
 			return this.WithQueryParam("top", n.ToString());
 		}
-		
-		public InsightlyRequest Skip(int n){
+
+		public InsightlyRequest Skip(int n)
+		{
 			return this.WithQueryParam("skip", n.ToString());
 		}
-		
-		public InsightlyRequest OrderBy(string order_by){
+
+		public InsightlyRequest OrderBy(string order_by)
+		{
 			return this.WithQueryParam("orderby", order_by);
 		}
-		
-		public InsightlyRequest Filter(string filter){
+
+		public InsightlyRequest Filter(string filter)
+		{
 			return this.WithQueryParam("filter", filter);
 		}
-		
-		public InsightlyRequest Filters(ICollection<string> filters){
-			foreach(var filter in filters){
+
+		public InsightlyRequest Filters(ICollection<string> filters)
+		{
+			foreach (var filter in filters)
+			{
 				this.Filter(filter);
 			}
 			return this;
 		}
 
-		private string QueryString{
-			get{
-				if(query_params.Count > 0){
+		private string QueryString
+		{
+			get
+			{
+				if (query_params.Count > 0)
+				{
 					return String.Join("&", query_params);
 				}
-				else{
+				else
+				{
 					return "";
 				}
 			}
 		}
-		
+
 		private const string BASE_URL = "https://api.insight.ly";
 		private HTTPMethod method;
 		private string api_key;
